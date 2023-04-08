639eb9170e9da7770ff7a1bbb7cd99e14d0e6751
peter <peterjschroeder@gmail.com>
2023-04-08 13:17:41 -0400

    try8

--- tuimoji/tuimoji.py
+++ tuimoji/tuimoji.py
@@ -32,7 +32,7 @@ class CustomSelectableIcon(urwid.SelectableIcon):
         return key
 
     def paste(self, contents):
-        p = Popen([shlex.split('nohup xclip -selection %s >/dev/null 2>&1' % self.selection)], stdin=PIPE)
+        p = Popen(['nohup', 'xclip', '-selection', self.selection, '> /dev/null 2>&1 &'], stdin=PIPE)
         p.communicate(contents)
 
 
